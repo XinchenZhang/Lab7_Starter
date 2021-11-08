24ba320ba9c37d59d8a80e8744ea3cfc5531b23e
XinchenZhang <54343729+XinchenZhang@users.noreply.github.com>
2021-11-08 00:01:47 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,28 +3,8 @@
 
 const CACHE_NAME = 'lab-7-starter';
 
-// Once the service worker has been installed, feed it some initial URLs to cache
-self.addEventListener('install', function (event) {
-  /**
-   * TODO - Part 2 Step 2
-   * Create a function as outlined above
-   */
-});
-
-/**
- * Once the service worker 'activates', this makes it so clients loaded
- * in the same scope do not need to be reloaded before their fetches will
- * go through this service worker
- */
-self.addEventListener('activate', function (event) {
-  /**
-   * TODO - Part 2 Step 3
-   * Create a function as outlined above, it shou// sw.js - This file needs to be in the root of the directory to work,
-//         so do not move it next to the other scripts
-
-const CACHE_NAME = 'lab-7-starter';
-
 var urlsToCache = [
+  
   "index.html",
   'assets/styles/main.css',
   'assets/scripts/main.js',
@@ -97,14 +77,4 @@ self.addEventListener('fetch', function (event) {
         );
       })
     );
-});ld be one line
-   */
-});
-
-// Intercept fetch requests and store them in the cache
-self.addEventListener('fetch', function (event) {
-  /**
-   * TODO - Part 2 Step 4
-   * Create a function as outlined above
-   */
 });
